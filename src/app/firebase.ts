cd3496409e2ce2ece82fb6d18c192aa29871e960
Jonas da Silva Santos <jonasuea@gmail.com>
2025-04-14 01:15:40 +0000

    apiKey: "AIzaSyBT3vLsTW-qWHUJOtlZ-lM1mcHxC1oDWTU",   authDomain: "diariodigital-e35c2.firebaseapp.com",   projectId: "diariodigital-e35c2",   storageBucket: "diariodigital-e35c2.firebasestorage.app",   messagingSenderId: "521750766445",   appId: "1:521750766445:web:ed6cc1eb16c6c9736f7064",   measurementId: "G-H6GQEPCTXE"

--- src/app/firebase.ts
+++ src/app/firebase.ts
@@ -4,12 +4,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+  apiKey: "AIzaSyBT3vLsTW-qWHUJOtlZ-lM1mcHxC1oDWTU",
+  authDomain: "diariodigital-e35c2.firebaseapp.com",
+  projectId: "diariodigital-e35c2",
+  storageBucket: "diariodigital-e35c2.firebasestorage.app",
+  messagingSenderId: "521750766445",
+  appId: "1:521750766445:web:ed6cc1eb16c6c9736f7064",
+  measurementId: "G-H6GQEPCTXE"
 };
 
 // Initialize Firebase
